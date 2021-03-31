cc3b3a8775732f7863d23e6eaa16ac04177f8c50
AM-UN <un.bulj@gmail.com>
2021-03-31 22:56:19 +0900

    質問の新規投稿機能の追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,27 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all.order(created_at: :desc)
   end
+
+  def new
+    @question = Question.new
+  end
+ 
+  def create
+    @question = Question.new(question_params)
+  
+    if @question.save
+      redirect_to("/questions")
+    else
+      render("questions/new")
+    end
+  end  
+
+
+  private
+   
+    def question_params
+      params.require(:question).permit(:question_content, :question_image)
+    end
+   
 end
