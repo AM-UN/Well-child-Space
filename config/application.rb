a898339996232fce9a8e2b21d9dc8590d5695b5e
AM-UN <un.bulj@gmail.com>
2021-04-01 22:29:28 +0900

    TimeZone設定をUTCからJSTに変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # Set time_zone from UTC to JST
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
