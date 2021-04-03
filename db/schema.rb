ce7f8ed257368672fd4a6a4939f68bfbf8448d78
AM-UN <un.bulj@gmail.com>
2021-04-03 23:38:14 +0900

    usersテーブルにname、profile_image、self_introductionカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_073901) do
+ActiveRecord::Schema.define(version: 2021_04_03_084953) do
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "question_content", null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2021_04_03_073901) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "profile_image"
+    t.string "self_introduciton"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
