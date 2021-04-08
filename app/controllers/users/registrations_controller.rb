84a7020b2b497aab4861e80cb1bbdf0fbd579701
AM-UN <un.bulj@gmail.com>
2021-04-08 15:06:13 +0900

    パスワード編集機能の追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -78,4 +78,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_update_path_for(resource)
+    user_path(current_user.id)
+  end
 end
