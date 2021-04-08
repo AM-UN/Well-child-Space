b4370f46220855e540e60a6833a554f07089f55c
AM-UN <un.bulj@gmail.com>
2021-04-08 23:07:08 +0900

    deviseの使用しない機能の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   validates :email, length:{minimum:5, maximum:30}, allow_blank: true
 
