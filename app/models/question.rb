2ccb699c3666caafe25bfc95142fcff4e1828e71
AM-UN <un.bulj@gmail.com>
2021-04-10 04:17:42 +0900

    質問一覧ページに投稿したユーザーの情報を追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,9 @@ class Question < ApplicationRecord
   validates :question_content, presence: true, length:{maximum:400}
   validates :user_id, presence: true
 
+  def user
+    return User.find_by(id:self.user_id)
+  end
+
   mount_uploader :question_image, ImageUploader
 end
