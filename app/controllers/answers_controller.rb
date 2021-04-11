2de97944fbeef6921dee6c12570eef38c6593ca5
AM-UN <un.bulj@gmail.com>
2021-04-11 20:25:58 +0900

    回答の新規投稿機能を追加

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,30 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+  
+  def new
+    @answer = Answer.new
+  end
+ 
+  def create
+    @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id
+    @answer.question_id = params[:id]
+    
+    if @answer.save
+      redirect_to(questions_path)
+    else
+      render action: :new
+    end
+  end  
+
+
+  private
+
+  def answer_params
+    params.permit(:answer_content, :answer_image)
+  end
+
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,9 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :user_id, presence: true
-  validates :answer_id, presence: true
+  validates :question_id, presence: true
   validates :answer_content, presence: true, length:{maximum:800}
+
+  mount_uploader :answer_image, ImageUploader
 
 end
