8ec093c32274a515c48919262753a9ef2deadff6
AM-UN <un.bulj@gmail.com>
2021-04-12 18:02:34 +0900

    質問に対するお気に入りの作成、削除機能の追加

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @favorite = Favorite.new(
+      user_id: current_user.id,
+      question_id: params[:question_id]
+      ) 
+    @favorite.save
+    redirect_to("/questions/#{params[:question_id]}")
+  end
+
+  def destroy
+    @favorite = Favorite.find_by(
+      user_id: current_user.id, 
+      question_id: params[:question_id]
+      )
+      @favorite.destroy
+    redirect_to("/questions/#{params[:question_id]}")
+  end 
+
+end
