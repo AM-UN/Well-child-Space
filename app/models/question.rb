a43348a790dbcc7ccf72b852090219b684661ad0
AM-UN <un.bulj@gmail.com>
2021-04-12 15:24:28 +0900

    favoritesテーブルの作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   validates :question_content, presence: true, length:{maximum:400}
   validates :user_id, presence: true
