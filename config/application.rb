a3c4cce0dbfe2f74e6ec2d952ff0565a5e14bc2d
AM-UN <un.bulj@gmail.com>
2021-04-24 04:46:24 +0900

    新規登録失敗時のデザインを整える

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module App
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
