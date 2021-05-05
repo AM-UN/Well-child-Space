a77c1245419b0db26c845670d3a70ecc770fa781
AM-UN <un.bulj@gmail.com>
2021-05-06 05:07:18 +0900

    質問一覧、質問詳細ページに表示する質問の投稿日を更新日に変更

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
-    @questions = Question.all.order(created_at: :desc)
+    @questions = Question.all.order(updated_at: :desc)
   end
 
   def new
