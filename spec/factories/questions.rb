11e942323dd4a4fe58c4ab06aee2f0aa5d3ddad5
AM-UN <un.bulj@gmail.com>
2021-05-27 01:12:43 +0900

    回答関連のE2Eテストの追加

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :question, class: Question do
-    question_content { '相談したいことがあります。'}
-    question_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images//default.jpg')) }
+    question_content { '相談したいことがあります。' }
+    question_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/default.jpg')) }
     user_id { create(:user).id }
   end
 end
